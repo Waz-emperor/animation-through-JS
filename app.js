fec63b4198dec5dc508205367ee3961d08368c36
Waz-emperor <64282899+Waz-emperor@users.noreply.github.com>
2020-07-30 15:11:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-$('.spoiler').hide();
+$('.spoilerButton').hide();
 
+$('.spoiler').on('click', function () {
+  $('.spoiler').hide();
+  $('.spoilerButton').fadeIn(2000);
+});
 $('.spoilerButton').on('click', function () {
   $('.spoilerButton').hide();
   $('.spoiler').slideDown(2000);
